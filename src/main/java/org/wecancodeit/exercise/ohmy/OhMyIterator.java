92ccf8eac250f127fa4422f2d87fcec42b342354
rbernstiel <rbernstiel@jpm-communications.com>
2017-02-02 14:45:41 -0500

    iterable

--- src/main/java/org/wecancodeit/exercise/ohmy/OhMyIterator.java
+++ src/main/java/org/wecancodeit/exercise/ohmy/OhMyIterator.java
@@ -1,4 +1,31 @@
 package org.wecancodeit.exercise.ohmy;
 
-public class OhMyIterator {
+import java.util.Iterator;
+import java.util.NoSuchElementException;
+
+public class OhMyIterator implements Iterator<String> {
+
+	private String[] elements;
+	private int curIndex;
+
+	public OhMyIterator(String[] elements) {
+		this.elements = elements;
+	}
+
+	@Override
+	public boolean hasNext() {
+	return curIndex<elements.length;
+	}
+	
+	
+
+	@Override
+	public String next() {
+		if(!hasNext()){
+			throw new NoSuchElementException();
+	}
+		String currentVal = elements[curIndex];
+		curIndex++;
+		return currentVal;
+	}
 }
